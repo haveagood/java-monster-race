6cfb4172624242565791be5f92d9c0e79a6a3908
haveagood@naver.com <haveagood@naver.com>
2020-02-05 16:53:21 +0900

    - 기존 step1 단계에서 구현된 클래스들 간의 의존성을 낮추고 있습니다.
        1. InputGameInfo클래스를 삭제했습니다.
    	- 대신 Game클래스 내부에 inputMonsterCount,
    	  inputGameCount메소드를 만들고, Game클래스의 생성자에 두
    	  메소드를 추가하여 Game메소드가 생성 될 때 자동으로 출력되게
    	  하였습니다.
    
    	- Game클래스 내부에 monsterCount, gameCount변수를 private하게
    	  가지고 있게 하였고, inputMonsterCount, inputGameCount 메소드
    	  내부에서 각 변수들을 집어넣게 하였습니다.

--- moster/src/main/java/MonsterRace/Game.java
+++ moster/src/main/java/MonsterRace/Game.java
@@ -1,20 +1,60 @@
 package MonsterRace;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Game {
-    InputGameInfo inputGameInfo;
-    Stadium stadium;
-    OutputGameResult outputGameResult;
+
+    Scanner scanner;
+    private int monsterCount;
+    private int gameCount;
+
+    public Game() {
+        inputMonsterCount();
+        inputGameCount();
+    }
+
+    public void setMonsterCount(int monsterCount) {
+        this.monsterCount = monsterCount;
+    }
+
+    public void setGameCount(int gameCount) {
+        this.gameCount = gameCount;
+    }
+
+    public void inputMonsterCount() {
+        scanner = new Scanner(System.in);
+        String monsterCountMessage = "<스릴만점 건전한 몬스터 경주>\n" + "몬스터는 모두 몇 마리인가요?";
+        System.out.println(monsterCountMessage);
+        setMonsterCount(Integer.parseInt(scanner.nextLine()));
+    }
+
+    public void inputGameCount() {
+        String gameCountMessage = "시도할 회수는 몇 회 인가요?";
+        System.out.println(gameCountMessage);
+        setGameCount(Integer.parseInt(scanner.nextLine()));
+        scanner.close();
+    }
+
+    public void printResult(GameInfo gameInfo) {
+        List<Monster> playerList = gameInfo.getPlayerList();
+        playerList.stream()
+                .forEach(monster -> {
+                    System.out.println(makeDash(monster));
+                });
+    }
+
+    public String makeDash(Monster monster) {
+        int totalMove = monster.getTotMove();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < totalMove; i++){
+            sb.append("-");
+        }
+        return sb.toString();
+    }
 
     public static void main(String[] args) {
         Game game = new Game();
-        game.start();
-    }
 
-    public void start() {
-        GameInfo gameInfo = new GameInfo();
-
-        inputGameInfo = new InputGameInfo(gameInfo);
-        stadium = new Stadium(gameInfo);
-        outputGameResult = new OutputGameResult(gameInfo);
     }
 }
