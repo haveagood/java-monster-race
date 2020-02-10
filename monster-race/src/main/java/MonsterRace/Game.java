61d9bc2c42d9284f91204a250a232e5226188290
haveagood@naver.com <haveagood@naver.com>
2020-02-10 16:09:04 +0900

    refactor : 불필요한 get,set메소드 제거
    - Game 클래스에서 불필요한 get,set메소드를 제거하였습니다.
    
    - Step3의 요구사항 중에서 몬스터 이름, 타입을 받는 메소드 추가

--- monster-race/src/main/java/MonsterRace/Game.java
+++ monster-race/src/main/java/MonsterRace/Game.java
@@ -1,5 +1,7 @@
 package MonsterRace;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Game {
@@ -7,10 +9,12 @@ public class Game {
     Scanner scanner;
     private int monsterCount;
     private int gameCount;
+    private List<String[]> monstersNameId = new ArrayList<>();
 
     public Game() {
         scanner = new Scanner(System.in);
         inputMonsterCount();
+        inputMonsterNameId();
         inputGameCount();
         scanner.close();
     }
@@ -28,20 +32,20 @@ public class Game {
     private void inputMonsterCount() {
         String monsterCountMessage = "<스릴만점 건전한 몬스터 경주>\n몬스터는 모두 몇 마리인가요?";
         System.out.println(monsterCountMessage);
-        setMonsterCount(Integer.parseInt(scanner.nextLine()));
+        monsterCount = Integer.parseInt(scanner.nextLine());
+    }
+
+    private void inputMonsterNameId() {
+        String monsterNameIdMessage = "경주할 몬스터 이름과 종류를 입력하세요 (쉼표(,)를 기준으로 구분).";
+        System.out.println(monsterNameIdMessage);
+        for(int i = 0; i < monsterCount; i++) {
+            monstersNameId.add(scanner.nextLine().split(","));
+        }
     }
 
     private void inputGameCount() {
         String gameCountMessage = "시도할 회수는 몇 회 인가요?";
         System.out.println(gameCountMessage);
-        setGameCount(Integer.parseInt(scanner.nextLine()));
-    }
-
-    public void setMonsterCount(int monsterCount) {
-        this.monsterCount = monsterCount;
-    }
-
-    public void setGameCount(int gameCount) {
-        this.gameCount = gameCount;
+        gameCount = Integer.parseInt(scanner.nextLine());
     }
 }
