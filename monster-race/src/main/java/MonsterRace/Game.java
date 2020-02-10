1715a416b8fa006445d9bf397d6a7d2f4b713e6d
haveagood@naver.com <haveagood@naver.com>
2020-02-10 15:36:07 +0900

    새로운 폴더에 코드 저장
    새로운 폴더에 step2구현까지의 코드를 저장하였습니다.

--- monster-race/src/main/java/MonsterRace/Game.java
+++ monster-race/src/main/java/MonsterRace/Game.java
@@ -1,4 +1,43 @@
 package MonsterRace;
 
+import java.util.Scanner;
+
 public class Game {
+
+    Scanner scanner;
+    private int monsterCount;
+    private int gameCount;
+
+    public Game() {
+        scanner = new Scanner(System.in);
+        inputMonsterCount();
+        inputGameCount();
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        Stadium stadium = new Stadium(game.monsterCount, game.gameCount);
+        stadium.printResult();
+    }
+
+    private void inputMonsterCount() {
+        String monsterCountMessage = "<스릴만점 건전한 몬스터 경주>\n몬스터는 모두 몇 마리인가요?";
+        System.out.println(monsterCountMessage);
+        setMonsterCount(Integer.parseInt(scanner.nextLine()));
+    }
+
+    private void inputGameCount() {
+        String gameCountMessage = "시도할 회수는 몇 회 인가요?";
+        System.out.println(gameCountMessage);
+        setGameCount(Integer.parseInt(scanner.nextLine()));
+    }
+
+    public void setMonsterCount(int monsterCount) {
+        this.monsterCount = monsterCount;
+    }
+
+    public void setGameCount(int gameCount) {
+        this.gameCount = gameCount;
+    }
 }
--- monster-race/src/main/java/MonsterRace/Monster.java
+++ monster-race/src/main/java/MonsterRace/Monster.java
@@ -1,4 +1,22 @@
 package MonsterRace;
 
 public class Monster {
+    private int monsterID;
+    private int totalMove;
+
+    public Monster(int monsterID) {
+        this.monsterID = monsterID;
+    }
+
+    public int getTotalMove() {
+        return totalMove;
+    }
+
+    public void setTotalMove(int totalMove) {
+        this.totalMove = totalMove;
+    }
+
+    public int getMonsterID() {
+        return this.monsterID;
+    }
 }
